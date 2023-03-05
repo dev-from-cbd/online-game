111a801ab07729a28e639434236b1bfa3df4d2d5
dev-from-cbd <veryfatwombat@gmail.com>
2023-03-05 08:52:16 +0800

    The Code of Game with comments

--- final.js
+++ final.js
@@ -1,33 +1,41 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting;
-let monterHealth;
-let inventory = ["stick"];
+/* ----------------------------------- */
+/*                                     */
+/*  This JavaScript Code developed     */
+/*  by Oleg Gribanov & ChatGPT :: 2023 */
+/*                                     */
+/* ----------------------------------- */
 
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterNameText = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
+let xp = 0; // определяем переменную xp и присваиваем ей значение 0
+let health = 100; // определяем переменную health и присваиваем ей значение 100
+let gold = 50; // определяем переменную gold и присваиваем ей значение 50
+let currentWeapon = 0; // определяем переменную currentWeapon и присваиваем ей значение 0
+let fighting; // определяем переменную fighting, но не присваиваем ей значение
+let monsterHealth; // определяем переменную monsterHealth, но не присваиваем ей значение
+let inventory = ["stick"]; // определяем переменную inventory и присваиваем ей массив с одним элементом "stick"
+
+const button1 = document.querySelector("#button1"); // определяем константу button1 и присваиваем ей элемент кнопки с id = "button1"
+const button2 = document.querySelector("#button2"); // определяем константу button2 и присваиваем ей элемент кнопки с id = "button2"
+const button3 = document.querySelector("#button3"); // определяем константу button3 и присваиваем ей элемент кнопки с id = "button3"
+const text = document.querySelector("#text"); // определяем константу text и присваиваем ей элемент с id = "text"
+const xpText = document.querySelector("#xpText"); // определяем константу xpText и присваиваем ей элемент с id = "xpText"
+const healthText = document.querySelector("#healthText"); // определяем константу healthText и присваиваем ей элемент с id = "healthText"
+const goldText = document.querySelector("#goldText"); // определяем константу goldText и присваиваем ей элемент с id = "goldText"
+const monsterStats = document.querySelector("#monsterStats"); // определяем константу monsterStats и присваиваем ей элемент с id = "monsterStats"
+const monsterNameText = document.querySelector("#monsterName"); // определяем константу monsterNameText и присваиваем ей элемент с id = "monsterName"
+const monsterHealthText = document.querySelector("#monsterHealth"); // определяем константу monsterHealthText и присваиваем ей элемент с id = "monsterHealth"
 
 const weapons = [
+  // определяем массив weapons
   {
-    name: "stick",
-    power: 5,
+    name: "stick", // определяем свойство name первого объекта массива weapons и присваиваем ему значение "stick"
+    power: 5, // определяем свойство power первого объекта массива weapons и присваиваем ему значение 5
   },
   {
-    name: "dagger",
-    power: 30,
+    name: "dagger", // определяем свойство name второго объекта массива weapons и присваиваем ему значение "dagger"
+    power: 30, // определяем свойство power второго объекта массива weapons и присваиваем ему значение 30
   },
   {
-    name: "claw hammer",
+    name: "claw hammer", // определяем свойство name третьего объекта
     power: 50,
   },
   {
@@ -36,6 +44,7 @@ const weapons = [
   },
 ];
 
+// Define the different monsters that the player can encounter, each with a name, level, and health.
 const monsters = [
   {
     name: "slime",
@@ -54,10 +63,11 @@ const monsters = [
   },
 ];
 
+// Define the different locations in the game, each with a name, text to display, and buttons with text and functions.
 const locations = [
   {
     name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button text": ["Go to store", "Go to cave", "Fight Zomby"],
     "button functions": [goStore, goCave, fightDragon],
     text: 'You are in the town square. You see a sign that says "Store."',
   },
@@ -103,7 +113,7 @@ const locations = [
     name: "win",
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
-    text: "You defeat the dragon! YOU WIN THE GAME! 🎉",
+    text: "You defeat the Zomby! YOU WIN THE GAME! 🎉",
   },
   {
     name: "easter egg",
@@ -119,46 +129,52 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  monsterStats.style.display = "none";
-  button1.innerText = location["button text"][0];
-  button2.innerText = location["button text"][1];
-  button3.innerText = location["button text"][2];
-  button1.onclick = location["button functions"][0];
-  button2.onclick = location["button functions"][1];
-  button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+  monsterStats.style.display = "none"; // Скрыть панель с информацией о монстре
+  button1.innerText = location["button text"][0]; // Изменить текст на первой кнопке на текст, указанный в объекте location
+  button2.innerText = location["button text"][1]; // Изменить текст на второй кнопке на текст, указанный в объекте location
+  button3.innerText = location["button text"][2]; // Изменить текст на третьей кнопке на текст, указанный в объекте location
+  button1.onclick = location["button functions"][0]; // Назначить функцию из массива button functions объекта location на первую кнопку
+  button2.onclick = location["button functions"][1]; // Назначить функцию из массива button functions объекта location на вторую кнопку
+  button3.onclick = location["button functions"][2]; // Назначить функцию из массива button functions объекта location на третью кнопку
+  text.innerText = location.text; // Изменить текст в блоке text на текст, указанный в объекте location
 }
 
 function goTown() {
-  update(locations[0]);
+  update(locations[0]); // Изменить текущее местоположение на город
 }
 
 function goStore() {
-  update(locations[1]);
+  update(locations[1]); // Изменить текущее местоположение на магазин
 }
 
 function goCave() {
-  update(locations[2]);
+  update(locations[2]); // Изменить текущее местоположение на пещеру
 }
 
 function buyHealth() {
   if (gold >= 10) {
-    gold -= 10;
-    health += 10;
-    goldText.innerText = gold;
-    healthText.innerText = health;
+    // Если у игрока достаточно золота для покупки здоровья
+    gold -= 10; // Убавить золото
+    health += 10; // Увеличить здоровье
+    goldText.innerText = gold; // Обновить текст в блоке goldText
+    healthText.innerText = health; // Обновить текст в блоке healthText
   } else {
-    text.innerText = "You do not have enough gold to buy health.";
+    // Если у игрока не достаточно золота для покупки здоровья
+    text.innerText = "You do not have enough gold to buy health."; // Вывести сообщение в блок text
   }
 }
 
+// Функция buyWeapon отвечает за покупку оружия. Если у игрока достаточно золота для покупки, он получает новое оружие и оно добавляется в его инвентарь. Если нет, то выводится сообщение об ошибке.
 function buyWeapon() {
   if (currentWeapon < weapons.length - 1) {
+    // Если у игрока не самое мощное оружие
     if (gold >= 30) {
-      gold -= 30;
-      currentWeapon++;
-      goldText.innerText = gold;
-      let newWeapon = weapons[currentWeapon].name;
+      // Если у игрока достаточно золота для покупки оружия
+      gold -= 30; // Убавить золото
+      currentWeapon++; // Переключиться на следующее оружие
+      goldText.innerText = gold; // Обновить текст в блоке goldText
+      let newWeapon = weapons[currentWeapon].name; // Запомнить название нового оружия
+
       text.innerText = "You now have a " + newWeapon + ".";
       inventory.push(newWeapon);
       text.innerText += " In your inventory you have: " + inventory;
@@ -172,6 +188,8 @@ function buyWeapon() {
   }
 }
 
+// Функция sellWeapon отвечает за продажу оружия. Если у игрока есть более одного оружия в инвентаре, то он продает текущее оружие и получает 15 золота. В противном случае выводится сообщение об ошибке.
+
 function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
@@ -183,6 +201,8 @@ function sellWeapon() {
     text.innerText = "Don't sell your only weapon!";
   }
 }
+
+// Функции fightSlime, fightBeast и fightDragon используются для выбора монстра, против которого будет сражаться игрок.
 
 function fightSlime() {
   fighting = 0;
@@ -199,6 +219,8 @@ function fightDragon() {
   goFight();
 }
 
+// Функция goFight выполняется при выборе монстра, инициализирует здоровье и статистику монстра, а также отображает это в интерфейсе.
+
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
@@ -206,6 +228,8 @@ function goFight() {
   monsterNameText.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
 }
+
+// Функция attack выполняет атаку на выбранного монстра. Если атака успешна, то здоровье игрока уменьшается на уровень атаки монстра. Затем выполняется атака на монстра, и его здоровье уменьшается на значение силы оружия игрока и случайное число между 1 и уровнем игрока. Если здоровье игрока или монстра меньше или равно 0, то вызывается соответствующая функция, либо игрок побеждает монстра и переходит к следующей локации.
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
@@ -234,83 +258,105 @@ function attack() {
   }
 }
 
+// Объявление функции getMonsterAttackValue, которая принимает аргумент level и вычисляет значение атаки монстра на основе его уровня
 function getMonsterAttackValue(level) {
+  // Вычисление значения удара монстра, исходя из уровня и случайного числа
   let hit = level * 5 - Math.floor(Math.random() * xp);
+  // Вывод в консоль значения удара
   console.log(hit);
+  // Возврат значения удара
   return hit;
 }
 
+// Объявление функции isMonsterHit, которая возвращает true или false в зависимости от того, попадет ли монстр по игроку
 function isMonsterHit() {
+  // Монстр попадет по игроку с вероятностью 80% (если Math.random() > 0.2) или если у игрока осталось меньше 20 очков здоровья
   return Math.random() > 0.2 || health < 20;
 }
 
+// Объявление функции dodge, которая обновляет текст игры, сообщая, что игрок увернулся от атаки монстра
 function dodge() {
+  // Обновление текста в элементе с id "text"
   text.innerText =
     "You dodge the attack from the " + monsters[fighting].name + ".";
 }
 
+// Объявление функции defeatMonster, которая обновляет количество золота и опыта игрока после убийства монстра, а также вызывает функцию update для обновления местоположения игрока
 function defeatMonster() {
+  // Увеличение количества золота игрока на значение, зависящее от уровня убитого монстра
   gold += Math.floor(monsters[fighting].level * 6.7);
+  // Увеличение количества опыта игрока на уровень убитого монстра
   xp += monsters[fighting].level;
+  // Обновление текста в элементе с id "goldText" с новым количеством золота
   goldText.innerText = gold;
+  // Обновление текста в элементе с id "xpText" с новым количеством опыта
   xpText.innerText = xp;
+  // Обновление местоположения игрока, вызывая функцию update с новым местоположением
   update(locations[4]);
 }
 
+// Объявление функции lose, которая вызывает функцию update с местоположением, соответствующим проигрышу
 function lose() {
+  // Обновление местоположения игрока, вызывая функцию update с новым местоположением
   update(locations[5]);
 }
 
+// Объявление функции winGame, которая вызывает функцию update с местоположением, соответствующим победе в игре
 function winGame() {
   update(locations[6]);
 }
 
 function restart() {
-  xp = 0;
-  health = 100;
-  gold = 50;
-  currentWeapon = 0;
-  inventory = ["stick"];
-  goldText.innerText = gold;
-  healthText.innerText = health;
-  xpText.innerText = xp;
-  goTown();
+  xp = 0; // Reset experience points to 0
+  health = 100; // Reset health to 100
+  gold = 50; // Reset gold to 50
+  currentWeapon = 0; // Reset current weapon to 0
+  inventory = ["stick"]; // Reset inventory to contain only a stick
+  goldText.innerText = gold; // Update the gold text element to display the current value of gold
+  healthText.innerText = health; // Update the health text element to display the current value of health
+  xpText.innerText = xp; // Update the experience points text element to display the current value of xp
+  goTown(); // Call the goTown() function to navigate to the town location
 }
 
 function easterEgg() {
-  update(locations[7]);
+  update(locations[7]); // Call the update() function with the location at index 7 to display an Easter egg
 }
 
 function pickTwo() {
-  pick(2);
+  pick(2); // Call the pick() function with the argument 2 to play the "Pick Two" game
 }
 
 function pickEight() {
-  pick(8);
+  pick(8); // Call the pick() function with the argument 8 to play the "Pick Eight" game
 }
 
 function pick(guess) {
-  let numbers = [];
+  let numbers = []; // Initialize an empty array to hold the random numbers
   while (numbers.length < 10) {
+    // Generate 10 random numbers between 0 and 10
     numbers.push(Math.floor(Math.random() * 11));
   }
 
-  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n"; // Update the text element to display the user's guess and the random numbers
 
   for (let i = 0; i < 10; i++) {
+    // Loop through the random numbers and add them to the text element
     text.innerText += numbers[i] + "\n";
   }
 
   if (numbers.indexOf(guess) !== -1) {
-    text.innerText += "Right! You win 20 gold!";
+    // If the user's guess is in the array of random numbers
+    text.innerText += "Right! You win 20 gold!"; // Update the text element to indicate the user won and award 20 gold
     gold += 20;
-    goldText.innerText = gold;
+    goldText.innerText = gold; // Update the gold text element to display the new value of gold
   } else {
-    text.innerText += "Wrong! You lose 10 health!";
+    // If the user's guess is not in the array of random numbers
+    text.innerText += "Wrong! You lose 10 health!"; // Update the text element to indicate the user lost and subtract 10 from health
     health -= 10;
-    healthText.innerText = health;
+    healthText.innerText = health; // Update the health text element to display the new value of health
     if (health <= 0) {
-      lose();
+      // If health is less than or equal to 0, the user has lost the game
+      lose(); // Call the lose() function to end the game
     }
   }
 }
