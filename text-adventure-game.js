d7a022785c63665092be96608c79947481a62a8e
dev-from-cbd <veryfatwombat@gmail.com>
2023-03-20 07:01:08 +0800

    The game

--- text-adventure-game.js
+++ text-adventure-game.js
@@ -8,16 +8,58 @@
 const textElement = document.getElementById("text");
 const optionButtonsElement = document.getElementById("option-buttons");
 
-function startGame(){
-    ...
+let state = {};
+
+function startGame() {
+  state = {};
+  showTextNode(1);
 }
 
-function showTextMode(textNodeIndex){
-    ...
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find((textNode) => textNode.id === textNodeIndex);
+  textElement.innerHTML = textNode.text;
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+
+  textNode.options.forEach((option) => {
+    /* if (showOption(option)){
+        const button = document.createElement("button");
+        button.innerText = option.text;
+        button.classList.add("btn");
+        button.addEventListener("click", () => selectOption(option));
+        optionButtonsElement.appendChild(button);
+    } */
+  });
 }
 
-function selectOption (option){
-    ...
+function selectOption(option) {
+  // 🇷🇺 В функции selectOption отсутствует блок кода, который изменяет state в соответствии с выбранной опцией. Необходимо добавить код, который изменяет state в соответствии с выбранной опцией.
+  // 🇷🇺 Также в функции selectOption отсутствует блок кода, который вызывает функцию showTextNode для отображения следующего узла с текстом. Необходимо добавить код, который вызывает функцию showTextNode для отображения следующего узла с текстом.
 }
 
-startGame()
+const textNodes = [
+  {
+    id: 1,
+    text: "You wake up",
+    options: [
+      {
+        text: "Take goo",
+        setState: {
+          blueGoo: true,
+        },
+        nextText: 2,
+      },
+      {
+        text: "Leave the goo",
+        nextText: 2,
+      },
+    ],
+  },
+  {
+    id: 2,
+    text: "...",
+  },
+];
+
+startGame();
