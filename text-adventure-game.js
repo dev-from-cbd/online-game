f972bd933e7604902b216a55356f0d9f9d24e38b
dev-from-cbd <veryfatwombat@gmail.com>
2023-03-19 08:01:51 +0800

    The game

--- text-adventure-game.js
+++ text-adventure-game.js
@@ -4,3 +4,20 @@
 /*  by Oleg Gribanov & ChatGPT :: 2023 */
 /*                                     */
 /* ----------------------------------- */
+
+const textElement = document.getElementById("text");
+const optionButtonsElement = document.getElementById("option-buttons");
+
+function startGame(){
+    ...
+}
+
+function showTextMode(textNodeIndex){
+    ...
+}
+
+function selectOption (option){
+    ...
+}
+
+startGame()
